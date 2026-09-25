384f3c5c1e8bba80dbe270b159232a0974d4379c
ihatwdslf <mariagrebenyuk0405@gmail.com>
2026-09-25 17:05:16 +0000

    Add main read loop

--- wish.c
+++ wish.c
@@ -2,5 +2,21 @@
 #include <stdlib.h>
 
 int main(void) {
-    return 0;
+    char *line = NULL;  // буфер
+    size_t cap = 0;     // розмір буфера
+
+    while (1) {
+        printf("wish> ");
+        fflush(stdout);
+
+        ssize_t len = getline(&line, &cap, stdin);
+        if (len == -1) {
+            break;      // EOF
+        }
+
+        printf("you typed: %s", line);  // debug
+    }
+
+    free(line);
+    exit(0);
 }
