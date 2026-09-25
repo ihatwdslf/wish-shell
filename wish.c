b3dfb23062f70cc2ae73e0cca2bb6dd08933aa28
ihatwdslf <mariagrebenyuk0405@gmail.com>
2026-09-25 17:16:39 +0000

    Add batch mode and argument checks

--- wish.c
+++ wish.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(void) {
+void print_error(void) {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+int main(int argc, char *argv[]) {
+    FILE *input = stdin;
+    int interactive = 1;
+
+    if (argc > 2) {
+        print_error();
+        exit(1);
+    }
+
+    if (argc == 2) {
+        input = fopen(argv[1], "r");
+        if (input == NULL) {
+            print_error();
+            exit(1);
+        }
+        interactive = 0;
+    }
+
     char *line = NULL;  // буфер
     size_t cap = 0;     // розмір буфера
 
     while (1) {
-        printf("wish> ");
-        fflush(stdout);
+        if (interactive) {
+            printf("wish> ");
+            fflush(stdout);
+        }
 
-        ssize_t len = getline(&line, &cap, stdin);
+        ssize_t len = getline(&line, &cap, input);
         if (len == -1) {
             break;      // EOF
         }
@@ -18,5 +44,8 @@ int main(void) {
     }
 
     free(line);
+    if (input != stdin) {
+        fclose(input);
+    }
     exit(0);
 }
